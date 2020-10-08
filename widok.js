7e701cf07b05561bf80670f715f51a3dc0ce9ce5
weterynarzfred <weterynarzfred@gmail.com>
2020-10-08 13:00:38 +0200

    add viewport height fix

--- widok.js
+++ widok.js
@@ -1,17 +1,21 @@
 const $ = require('cash-dom');
 const throttle = require('widok-throttle');
 
-const bodyElement = $('body');
+const $window = $(window);
+const $body = $('body');
 let isPageScrolled = false;
+
 const widok = {
   h: 0,
   w: 0,
   s: 0,
-  em: 0,
   sizeCheck: () => {
-    widok.h = $(window).height();
-    widok.w = $(window).width();
-    widok.em = parseFloat($('body').css('font-size'));
+    widok.h = $window.height();
+    widok.w = $window.width();
+    document.documentElement.style.setProperty(
+      '--vh',
+      `${window.innerHeight / 100}px`
+    );
     window.dispatchEvent(new CustomEvent('layoutChange'));
     widok.scrollCheck();
     window.dispatchEvent(new CustomEvent('afterLayoutChange'));
@@ -20,27 +24,24 @@ const widok = {
     widok.s = window.scrollY;
     if (widok.s > 10) {
       if (!isPageScrolled) {
-        bodyElement.addClass('scrolled');
+        $body.addClass('scrolled');
         isPageScrolled = true;
       }
     } else {
       if (isPageScrolled) {
-        bodyElement.removeClass('scrolled');
+        $body.removeClass('scrolled');
         isPageScrolled = false;
       }
     }
   },
 };
 
-$(window).on({
+$window.on({
   resize: throttle(100, widok.sizeCheck),
   load: widok.sizeCheck,
   scroll: widok.scrollCheck,
 });
 
-$(document).on('ready', widok.sizeCheck);
-$(document).on('ready', () => {
-  console.log('ready');
-});
+document.addEventListener('ready', widok.sizeCheck);
 
 if (typeof module !== 'undefined') module.exports = widok;
